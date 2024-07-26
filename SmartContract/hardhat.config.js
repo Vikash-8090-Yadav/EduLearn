571638751a0e71fa649553142c55424a0211282a
Vikash <yadav621gzp@gmail.com>
2024-07-26 09:42:44 +0530

    Deleted ols Artificats & cahche

--- SmartContract/hardhat.config.js
+++ SmartContract/hardhat.config.js
@@ -5,13 +5,13 @@ require ('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: "0.8.10",
 
-  defaultNetwork: "L3",
+  defaultNetwork: "Edu",
   networks:{
     hardhat:{},
-    L3: {
-      url: "https://rpc-live-amber-cougar-9xs8t1or8j.t.conduit.xyz",
-      chainId: 51611,
-      accounts: ['5753e65f56865a161fbf41932a0d855139a4ce9dc20d82fb655bff393fc41702'],
+    Edu: {
+      url: "https://rpc.open-campus-codex.gelato.digital/",
+      chainId: 656476,
+      accounts: ['a77637fc7905e02b1ec4b9e913cc895b627be58053f2fdd774911623004c3285'],
     },
   }
 };
